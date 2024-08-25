ead59df531cc2ac702e149a61554d512b08e7b53
iamvarunkumar2k4 <varunkumar17135176@gmail.com>
2024-08-25 20:53:56 +0530

    Create pattern1.cpp

---
+++ pattern1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    int i;
+    int j;
+    cout<<x;
+    cin>>x;
+    for(i=1;i<=x;i++)
+    {
+        for(j=1;j<=x;j++)
+    {
+        cout<<"*";
+        
+    }
+    cout<<"\n";
+    }
+    
+
+    return 0;
+}
